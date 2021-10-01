3ee510b9ad3b001c3da1d60473db5fe5ce6e529f
Angel Fernandes <angelfernandes07@hotmail.com>
2021-10-01 09:15:19 -0300

    initial config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import Logo from '../assets/avatar.png'
 import Animal from '../assets/animal.svg'
 import * as S from '../styles/pages/home'
 
@@ -9,18 +8,7 @@ const Home: React.FC = () => {
   const [teste] = useState('Setup do Projeto')
   return (
     <S.Container>
-      <Head>
-        <title>Página Home</title>
-      </Head>
-      <S.Content>
-        <Image src={Logo} alt="teste" width={200} height={200} />
-        <h1>{teste}</h1>
-        <S.WrapperSvg>
-          <Animal />
-        </S.WrapperSvg>
-
-        <p>Estrutura inicial com Next JS 10</p>
-      </S.Content>
+      <h1>Meu projecto</h1>
     </S.Container>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -8,33 +8,3 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
 `
-export const Content = styled.main`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  h1 {
-    font-size: 54px;
-    color: ${props => props.theme.colors.primary};
-    margin-top: 40px;
-  }
-
-  p {
-    margin-top: 24px;
-    font-size: 24px;
-    line-height: 32px;
-  }
-`
-
-export const WrapperSvg = styled.div`
-  width: 100px;
-  height: 100px;
-  text-align: center;
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-`
